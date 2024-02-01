c0532902b3c441c0b06cc452af6ddc113e11a838
Yazid Sameh <127590607+yazidsameh@users.noreply.github.com>
2024-02-01 21:57:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+<div>
+  <h1 class="slogen">Your Vision, Our Design</h1>
+</div>
+<ul>
+  <li>
+    <span class="fas fa-home" id="headIcon"></span>
+    <a href="#main"> Home </a>
+  </li>
+  <li>
+    <span class="fa-regular fa-image" id="headIcon"></span>
+    <a href="#services"> Services </a>
+  </li>
+  <li>
+    <span class="fas fa-question-circle" id="headIcon"></span>
+    <a href="#"> Help </a>
+  </li>
+  <li>
+    <span class="fa-solid fa-phone" id="headIcon"></span>
+    <a href="#"> Contact </a>
+  </li>
+</ul>
